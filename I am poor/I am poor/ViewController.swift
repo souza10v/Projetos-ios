2aee461693feb005b04f5608e5619829b95bd382
Donizetti Aparecido de Souza Jr <71847472+souza10v@users.noreply.github.com>
2022-08-16 15:17:50 -0300

    Add files via upload

---
+++ I am poor/I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Donizetti de Souza on 8/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
