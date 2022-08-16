41bbc34546f853dba990bf524bbd8a2830a57eb7
Donizetti Aparecido de Souza Jr <71847472+souza10v@users.noreply.github.com>
2022-08-16 15:19:41 -0300

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Donizetti de Souza on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
