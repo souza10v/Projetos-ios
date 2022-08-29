2e194325e69a47ae2bfb8d7f9d9c3999b2e58ad8
Donizetti Aparecido de Souza Jr <71847472+souza10v@users.noreply.github.com>
2022-08-29 17:53:34 -0300

    Update AppDelegate.swift

--- Magic-8-Ball/Magic 8 Ball/AppDelegate.swift
+++ Magic-8-Ball/Magic 8 Ball/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Magic 8 Ball
-//
-//  Created by Angela Yu on 14/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
