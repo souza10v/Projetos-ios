7101a401af6ce0d215812b0ad5158c90d89b3296
Donizetti Aparecido de Souza Jr <71847472+souza10v@users.noreply.github.com>
2022-08-29 17:53:03 -0300

    Update SceneDelegate.swift

--- EggTimer/EggTimer/SceneDelegate.swift
+++ EggTimer/EggTimer/SceneDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  SceneDelegate.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
